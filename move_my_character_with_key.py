979631986ac109c92ad47deb70a4225317e68c81
hyunholee <dionme2622@gmail.com>
2023-09-28 16:51:56 +0900

    Move_Left 함수 생성 완료

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,11 +6,23 @@ tuk_ground = load_image('TUK_GROUND.png')
 character_idle = load_image('Pinkbean_Idle.png')
 character_move = load_image('Pinkbean_Move.png')
 
+direction = 'RIGHT'
 running = True
-x, y, diff = 0, 0, 0
+frame, x, y, diff = 0, 0, 0, 0
+a, b, c = 0, 0, 0
 def Move_Left():
-    global x
+    global x, frame, a, b, c
     x = x - diff
+    if frame < 3:
+        character_move.clip_composite_draw(0 + a * 102, 0, 113, 126, 0, 'h', 640, 512, 100, 100)
+        a = (a + 1) % 3
+    elif frame < 5:
+        character_move.clip_composite_draw(306 + b * 124, 0, 113, 126, 0, 'h', 640, 512, 100, 100)
+        b = (b + 1) % 2
+    elif frame < 9:
+        character_move.clip_composite_draw(554 + c * 110, 0, 113, 126, 0, 'h', 640, 512, 100, 100)
+        c = (c + 1) % 4
+    frame = (frame + 1) % 9
 def Move_Right():
     global x
     x = x + diff
@@ -25,7 +37,7 @@ def Move_Down():
 def Idle():
     pass
 def handle_events():
-    global running
+    global running, direction
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -33,8 +45,10 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_LEFT:
                 Move_Left()
+                direction = 'LEFT'
             elif event.key == SDLK_RIGHT:
                 Move_Right()
+                direction = 'RIGHT'
             elif event.key == SDLK_UP:
                 Move_Up()
             elif event.key == SDLK_DOWN:
@@ -53,6 +67,7 @@ def handle_events():
 
 
 while running:
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     handle_events()
     pass
 
